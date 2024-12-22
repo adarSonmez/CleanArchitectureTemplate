a40f9667d9f6fd21c94d6d6025fb09a9b202a2eb
adarSonmez <adarsonmez@outlook.com>
2024-12-22 14:52:08 +0300

    Add entity membership and ordering configurations

---
+++ Infrastructure/CommercePortal.Persistence/Configurations/EntityFramework/Membership/CustomerConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using CommercePortal.Domain.Entities.Membership;
+using CommercePortal.Persistence.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CommercePortal.Persistence.Configurations.EntityFramework.Membership;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        // Table Configuration
+        builder.ToTable("Customers", "Membership");
+
+        // Property Configurations
+        builder.Property(c => c.Age)
+            .HasDefaultValue(null)
+            .HasColumnType("smallint");
+
+        builder.Ignore(c => c.DomainUser);
+
+        // Value Converters
+        builder.Property(c => c.Gender)
+               .HasConversion<string>();
+
+        // Relationships
+        builder.HasOne<AppUser>()
+               .WithOne()
+               .HasForeignKey<Customer>(c => c.UserId)
+               .IsRequired();
+    }
+}
---
+++ Infrastructure/CommercePortal.Persistence/Configurations/EntityFramework/Membership/StoreConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using CommercePortal.Domain.Entities.Membership;
+using CommercePortal.Persistence.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CommercePortal.Persistence.Configurations.EntityFramework.Membership;
+
+/// <summary>
+/// EF Core configuration for the <see cref="Store"/> entity.
+/// </summary>
+public class StoreConfiguration : IEntityTypeConfiguration<Store>
+{
+    public void Configure(EntityTypeBuilder<Store> builder)
+    {
+        // Table Configuration
+        builder.ToTable("Stores", "Membership");
+
+        // Property Configurations
+        builder.Property(s => s.Website)
+               .HasMaxLength(200) // Limit URL length to 200 characters
+               .IsRequired(false);
+
+        builder.Property(s => s.Description)
+               .HasMaxLength(1000) // Limit description to 1000 characters
+               .IsRequired(false);
+
+        builder.Ignore(s => s.DomainUser);
+
+        // Relationships
+        builder.HasOne<AppUser>()
+               .WithOne()
+               .HasForeignKey<Store>(s => s.UserId)
+               .IsRequired();
+    }
+}
