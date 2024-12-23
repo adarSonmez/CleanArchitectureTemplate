76fc81b7277a8178b1f004fb2bcee2e69c51456a
adarSonmez <adarsonmez@outlook.com>
2024-12-23 19:00:01 +0300

    Refactor service registrations for new repositories

--- Infrastructure/CommercePortal.Persistence/ServiceRegistration.cs
+++ Infrastructure/CommercePortal.Persistence/ServiceRegistration.cs
@@ -1,13 +1,15 @@
-﻿using CommercePortal.Application.Repositories.Customers;
-using CommercePortal.Application.Repositories.Files;
-using CommercePortal.Application.Repositories.Orders;
-using CommercePortal.Application.Repositories.Products;
-using CommercePortal.Domain.Entities.Identity;
+﻿using CommercePortal.Application.Abstractions.Repositories.Files;
+using CommercePortal.Application.Abstractions.Repositories.Marketing;
+using CommercePortal.Application.Abstractions.Repositories.Membership;
+using CommercePortal.Application.Abstractions.Repositories.Ordering;
+using CommercePortal.Application.Abstractions.Services.Data;
 using CommercePortal.Persistence.Contexts;
-using CommercePortal.Persistence.Repositories.EntityFramework.Customers;
+using CommercePortal.Persistence.Identity;
 using CommercePortal.Persistence.Repositories.EntityFramework.Files;
-using CommercePortal.Persistence.Repositories.EntityFramework.Orders;
-using CommercePortal.Persistence.Repositories.EntityFramework.Products;
+using CommercePortal.Persistence.Repositories.EntityFramework.Marketing;
+using CommercePortal.Persistence.Repositories.EntityFramework.Membership;
+using CommercePortal.Persistence.Repositories.EntityFramework.Ordering;
+using CommercePortal.Persistence.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -54,19 +56,52 @@ public static class ServiceRegistration
 
         #region Repository services
 
-        services.AddScoped<IProductReadRepository, EfProductReadRepository>();
-        services.AddScoped<IProductWriteRepository, EfProductWriteRepository>();
-        services.AddScoped<IOrderReadRepository, EfOrderReadRepository>();
-        services.AddScoped<IOrderWriteRepository, EfOrderWriteRepository>();
-        services.AddScoped<ICustomerReadRepository, EfCustomerReadRepository>();
-        services.AddScoped<ICustomerWriteRepository, EfCustomerWriteRepository>();
-        services.AddScoped<IFileReadRepository, EfFileReadRepository>();
-        services.AddScoped<IFileWriteRepository, EfFileWriteRepository>();
+        // Files
+        services.AddScoped<ICategoryImageFileReadRepository, EfCategoryImageFileReadRepository>();
+        services.AddScoped<ICategoryImageFileWriteRepository, EfCategoryImageFileWriteRepository>();
+        services.AddScoped<IFileDetailsReadRepository, EfFileDetailsReadRepository>();
+        services.AddScoped<IFileDetailsWriteRepository, EfFileDetailsWriteRepository>();
         services.AddScoped<IInvoiceFileReadRepository, EfInvoiceFileReadRepository>();
         services.AddScoped<IInvoiceFileWriteRepository, EfInvoiceFileWriteRepository>();
         services.AddScoped<IProductImageFileReadRepository, EfProductImageFileReadRepository>();
         services.AddScoped<IProductImageFileWriteRepository, EfProductImageFileWriteRepository>();
+        services.AddScoped<IReportFileReadRepository, EfReportFileReadRepository>();
+        services.AddScoped<IReportFileWriteRepository, EfReportFileWriteRepository>();
+        services.AddScoped<IUserAvatarFileReadRepository, EfUserAvatarFileReadRepository>();
+        services.AddScoped<IUserAvatarFileWriteRepository, EfUserAvatarFileWriteRepository>();
+
+        // Marketing
+        services.AddScoped<ICategoryReadRepository, EfCategoryReadRepository>();
+        services.AddScoped<ICategoryWriteRepository, EfCategoryWriteRepository>();
+        services.AddScoped<IProductReadRepository, EfProductReadRepository>();
+        services.AddScoped<IProductWriteRepository, EfProductWriteRepository>();
+
+        // Membership
+        services.AddScoped<ICustomerReadRepository, EfCustomerReadRepository>();
+        services.AddScoped<ICustomerWriteRepository, EfCustomerWriteRepository>();
+        services.AddScoped<IStoreReadRepository, EfStoreReadRepository>();
+        services.AddScoped<IStoreWriteRepository, EfStoreWriteRepository>();
+
+        // Ordering
+        services.AddScoped<IInvoiceReadRepository, EfInvoiceReadRepository>();
+        services.AddScoped<IInvoiceWriteRepository, EfInvoiceWriteRepository>();
+        services.AddScoped<IOrderItemReadRepository, EfOrderItemReadRepository>();
+        services.AddScoped<IOrderItemWriteRepository, EfOrderItemWriteRepository>();
+        services.AddScoped<IOrderReadRepository, EfOrderReadRepository>();
+        services.AddScoped<IOrderWriteRepository, EfOrderWriteRepository>();
 
         #endregion Repository services
+
+        #region Custom services
+
+        services.AddScoped<IDataService, EfDataService>();
+
+        #endregion Custom services
+
+        #region Mapping services
+
+        services.AddAutoMapper(typeof(IdentityMappingProfile).Assembly, typeof(Application.ServiceRegistration).Assembly);
+
+        #endregion Mapping services
     }
 }
