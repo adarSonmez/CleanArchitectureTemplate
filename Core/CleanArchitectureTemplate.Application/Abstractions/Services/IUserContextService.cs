f5f4a91c3ae908ae70f34cc91a5732b1f56462f4
adarSonmez <adarsonmez@outlook.com>
2025-03-01 00:21:30 +0300

    Add IUserContextService; update required properties

---
+++ Core/CleanArchitectureTemplate.Application/Abstractions/Services/IUserContextService.cs
@@ -0,0 +1,17 @@
+﻿namespace CleanArchitectureTemplate.Application.Abstractions.Services;
+
+/// <summary>
+/// Provides information about the currently authenticated user.
+/// </summary>
+public interface IUserContextService
+{
+    /// <summary>
+    /// Gets the current user's username.
+    /// </summary>
+    string? GetUserName();
+
+    /// <summary>
+    /// Gets the current user's unique identifier.
+    /// </summary>
+    Guid? GetUserId();
+}
