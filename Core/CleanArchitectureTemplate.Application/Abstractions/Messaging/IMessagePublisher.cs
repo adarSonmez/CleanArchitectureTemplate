7b88151f466fc0ca011e0cf9c6bb4a91e3ef821d
adarSonmez <adarsonmez@outlook.com>
2025-04-15 21:04:11 +0300

    Add messaging interfaces for publishing and subscribing

---
+++ Core/CleanArchitectureTemplate.Application/Abstractions/Messaging/IMessagePublisher.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitectureTemplate.Application.Dtos.Messaging.PubSub;
+
+namespace CleanArchitectureTemplate.Application.Abstractions.Messaging;
+
+/// <summary>
+/// Abstraction for publishing messages to a messaging system.
+/// </summary>
+public interface IMessagePublisher
+{
+    /// <summary>
+    /// Publishes the given message asynchronously.
+    /// </summary>
+    /// <typeparam name="T">The message type.</typeparam>
+    /// <param name="messageDto">The message to be published.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task PublishAsync<T>(PublishMessageDto<T> messageDto) where T : class;
+}
---
+++ Core/CleanArchitectureTemplate.Application/Abstractions/Messaging/IMessageSubscriber.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitectureTemplate.Application.Dtos.Messaging.PubSub;
+
+namespace CleanArchitectureTemplate.Application.Abstractions.Messaging;
+
+/// <summary>
+/// Abstraction for subscribing and processing messages from a messaging system.
+/// </summary>
+public interface IMessageSubscriber
+{
+    /// <summary>
+    /// Subscribes to messages of type T using the specified routing key.
+    /// </summary>
+    /// <typeparam name="T">The type of the message.</typeparam>
+    /// <param name="messageDto">The message subscription details.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task<string> SubscribeAsync<T>(SubscribeMessageDto<T> messageDto) where T : class;
+
+    /// <summary>
+    /// Unsubscribes from messages using the specified consumer tag.
+    /// </summary>
+    public Task UnsubscribeAsync(string consumerTag, CancellationToken cancellationToken = default);
+}
