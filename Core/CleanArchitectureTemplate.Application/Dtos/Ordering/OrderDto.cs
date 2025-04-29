7964e84082db68103649e9f1962159ff86a1f081
adarSonmez <adarsonmez@outlook.com>
2025-04-29 23:48:58 +0300

    Remove OrderItemDto used BasketDto instead

--- Core/CleanArchitectureTemplate.Application/Dtos/Ordering/OrderDto.cs
+++ Core/CleanArchitectureTemplate.Application/Dtos/Ordering/OrderDto.cs
@@ -1,7 +1,7 @@
-﻿using CleanArchitectureTemplate.Domain.Constants.Enums;
+﻿using CleanArchitectureTemplate.Application.Dtos.Shopping;
+using CleanArchitectureTemplate.Domain.Constants.Enums;
 using CleanArchitectureTemplate.Domain.Entities.Ordering;
 using CleanArchitectureTemplate.Domain.MarkerInterfaces;
-using CleanArchitectureTemplate.Domain.ValueObjects;
 
 namespace CleanArchitectureTemplate.Application.Dtos.Ordering;
 
@@ -10,18 +10,10 @@ namespace CleanArchitectureTemplate.Application.Dtos.Ordering;
 /// </summary>
 /// <param name="Id">The unique identifier.</param>
 /// <param name="Status">The status of the order.</param>
-/// <param name="TotalAmount">The total amount of the order.</param>
-/// <param name="ShippingAddress">The shipping address of the order.</param>
-/// <param name="CustomerId">The Id of the customer who placed the order.</param>
-/// <param name="BasketId">The Id of the basket associated with the order.</param>
-/// <param name="OrderItems">The order items of the order.</param>
+/// <param name="Basket">The basket associated with the order.</param>
 public record OrderDto
 (
     Guid Id = default,
     OrderStatus Status = default,
-    Money? TotalAmount = default,
-    Address ShippingAddress = default!,
-    Guid CustomerId = default,
-    Guid BasketId = default,
-    ICollection<OrderItemDto>? OrderItems = default
+    BasketDto? Basket = default
 ) : IDto;
--- Core/CleanArchitectureTemplate.Application/Mappings/Ordering/OrderMapper.cs
+++ Core/CleanArchitectureTemplate.Application/Mappings/Ordering/OrderMapper.cs
@@ -1,7 +1,7 @@
 ﻿using CleanArchitectureTemplate.Application.Dtos.Ordering;
 using CleanArchitectureTemplate.Application.Features.Orders.Commands.CreateOrderFromBasket;
+using CleanArchitectureTemplate.Application.Mappings.Shopping;
 using CleanArchitectureTemplate.Domain.Entities.Ordering;
-using CleanArchitectureTemplate.Domain.ValueObjects;
 
 namespace CleanArchitectureTemplate.Application.Mappings.Ordering;
 
@@ -31,22 +31,15 @@ public static class OrderMapper
     /// Maps an <see cref="Order"/> entity to an <see cref="OrderDto"/>.
     /// </summary>
     /// <param name="order">The order entity to map.</param>
-    /// <param name="totalAmount">The calculated total amount of the order.</param>
-    /// <param name="customerId">The ID of the customer who placed the order.</param>
-    /// <param name="orderItems">The collection of order items for the order.</param>
     /// <returns>The mapped <see cref="OrderDto"/>.</returns>
-    public static OrderDto ToDto(this Order order, Money? totalAmount = null, Guid customerId = default, ICollection<OrderItemDto>? orderItems = null)
+    public static OrderDto ToDto(this Order order)
     {
         if (order == null) return null!;
 
         return new OrderDto(
             Id: order.Id,
             Status: order.Status,
-            TotalAmount: totalAmount,
-            ShippingAddress: order.ShippingAddress!,
-            CustomerId: customerId,
-            BasketId: order.BasketId,
-            OrderItems: orderItems
+            Basket: order.Basket?.ToDto()
         );
     }
 }
